088c5a57a47fdd5945fbf8b6a516b73a8ed395f2
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2020-01-14 22:51:39 -0300

    Query e Route params

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello Word" });
+//Query params = ?teste=1
+//Router params = /users/1
+//Request body = { }
+
+server.get("/users/:id", (req, res) => {
+  //const nome = req.query.nome;
+  const { id } = req.params;
+
+  //return res.json({ message: `Hello ${nome}` });
+  return res.json({ message: `Buscando o Usuario ${id}` });
 });
 
 server.listen(3000);
